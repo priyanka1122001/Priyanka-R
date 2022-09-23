3e51030d48b3c7db4b996d7a8998d7bf94f9fe5e
priyanka1122001 <113168879+priyanka1122001@users.noreply.github.com>
2022-09-23 12:24:56 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+//Priyanka R CG 2576
+
+//print all the variables
+
+class Datatypes // class creation, class name "variable"
+{
+   public static void main(String[] args)//main method
+   {
+       int x=4; //integer declaration & assignment
+       float y=45.6f; //integer declaration & assignment
+       double d=78.4; //float declaration & assignment
+       char z='b';    //character declaration & assignment
+       String name="Priyanka"; //string declaration & assignment
+       boolean b=true;  //boolean declaration & assignment
+       byte c=10;     // byte declaration & assignment
+       short s=24;   //short declaration & assignment
+       long l1=100;  //long declaration & assignment
+       System.out.println(x+"  "+y+" "+d+"  "+z+"  "+name+"  "+b+"  "+c+"  "+s+"  "+l1);//print all the types 
+    }
+}
