e5d2a67d8de5e811c95b5578693fbaa8ad2b274e
priyanka1122001 <113168879+priyanka1122001@users.noreply.github.com>
2022-10-05 17:17:58 +0530

    Add files via upload

---
+++ Inputfunctions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Inputfunctions 
+{
+  public static void main(String[] args)
+  {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter Two Numbers (Press Enter after each):");
+   //two variables to hold numbers
+	   if(sc==y)
+	  {
+   double n1, n2, n3;
+   n1 = sc.nextDouble();
+   n2 = sc.nextDouble();
+   n3 = n1 + n2;
+   System.out.println("Total = " + n3);
+   System.out.println("Continue(Y/N)");
+	  }
+      
+  }
+}
